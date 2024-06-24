11b74c15c72a6f65b780b82939048f40d3e47667
zhdaniukivan <iz2819815@gmail.com>
2024-06-24 20:28:23 +0300

    init project

---
+++ gpt_manager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GptManagerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gpt_manager'
---
+++ gpt_manager/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from openai import OpenAI
+
+client = OpenAI(
+    organization='',
+    project='',
+)
