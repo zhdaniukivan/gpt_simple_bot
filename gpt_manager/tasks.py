f499875696baf5581dcb03805f054675b942d605
zhdaniukivan <iz2819815@gmail.com>
2024-07-05 21:30:37 +0300

    add decorator to check time func.

--- gpt_manager/tasks.py
+++ gpt_manager/tasks.py
@@ -6,3 +6,5 @@ app = Celery('gpt_simple_bot', broker='redis://localhost:6379/0')
 @app.task
 def add(x, y):
     return x + y
+
+
--- gpt_manager/views.py
+++ gpt_manager/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from openai import OpenAI
+import time
 
-client = OpenAI(
-    organization='',
-    project='',
-)
+# client = OpenAI(
+#     organization='',
+#     project='',
+# )
+
+
+def time_check(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print("time fuc:", time.time() - start)
+        return result
+    return wrapper
+
+
+simple_print()
